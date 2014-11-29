4fbb29bf068797b33b6c873d1eca43b1168ca303
fkenk <fkenkandfkenk@gmail.com>
2014-11-29 15:58:38 +0200

    aaaaaaaaaaaaaaaaaa

--- src/forms/customer/CustomerForm.java
+++ src/forms/customer/CustomerForm.java
@@ -49,7 +49,7 @@ public class CustomerForm extends JPanel implements Fill {
             e.consume();
             System.out.println("dafdf");
             System.out.println("dfsdfdsfsdf");
-            System.out.println("");
+            System.out.println("aaaaaaaaaaaaaaaaaaaa");
         }
     }
 
